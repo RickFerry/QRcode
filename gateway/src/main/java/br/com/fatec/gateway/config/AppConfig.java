9bd4863eb5be847e9d520663392c005bea6563a2
RickFerry <rick_ferry1@hotmail.com>
2023-09-10 14:51:15 -0300

    Refactor code oauth project.

--- gateway/src/main/java/br/com/fatec/gateway/config/AppConfig.java
+++ gateway/src/main/java/br/com/fatec/gateway/config/AppConfig.java
@@ -3,23 +3,9 @@ package br.com.fatec.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("MY-SECRET-KEY");
-        return converter;
-    }
-
-    @Bean
-    JwtTokenStore jwtTokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
-    }
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
