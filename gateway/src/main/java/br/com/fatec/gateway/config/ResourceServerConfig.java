3d98eb465dcf19ed3a7ca587bfe35de2c99cb77f
RickFerry <rick_ferry1@hotmail.com>
2023-09-10 13:36:22 -0300

    Refresh token funcionando e cookie(não gerando o cookie).

--- gateway/src/main/java/br/com/fatec/gateway/config/ResourceServerConfig.java
+++ gateway/src/main/java/br/com/fatec/gateway/config/ResourceServerConfig.java
@@ -1,13 +1,18 @@
 package br.com.fatec.gateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
+@EnableWebSecurity
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
@@ -17,10 +22,18 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
         this.store = store;
     }
 
+    @Autowired
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources
-                .tokenStore(store);
+        resources.stateless(true);
     }
 
     @Override
@@ -30,6 +43,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/ms-oauth/oauth/token").permitAll()
 //                .antMatchers("").hasAnyRole("")
 //                .antMatchers("").hasRole("")
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable();
     }
 }
