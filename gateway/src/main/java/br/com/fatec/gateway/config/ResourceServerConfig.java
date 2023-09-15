6b1d9b7ac0ba9820a9eac5c662067abb4095c889
RickFerry <rick_ferry1@hotmail.com>
2023-09-14 23:57:11 -0300

    Path authorization.

--- gateway/src/main/java/br/com/fatec/gateway/config/ResourceServerConfig.java
+++ gateway/src/main/java/br/com/fatec/gateway/config/ResourceServerConfig.java
@@ -34,7 +34,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(ResourceServerSecurityConfigurer resources) {
         resources.stateless(true);
     }
 
