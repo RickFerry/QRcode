4db0b25f63416a83b6f374611f76abc8d02830cf
RickFerry <rick_ferry1@hotmail.com>
2023-10-22 21:38:21 -0300

    Classe UserService testada.

--- ms-user/src/main/java/br/com/fatec/user/service/UserService.java
+++ ms-user/src/main/java/br/com/fatec/user/service/UserService.java
@@ -3,7 +3,6 @@ package br.com.fatec.user.service;
 import br.com.fatec.user.model.Usuario;
 import br.com.fatec.user.model.dto.UsuarioDto;
 import br.com.fatec.user.repository.UserRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,7 +38,6 @@ public class UserService {
     @Transactional
     public UsuarioDto create(UsuarioDto dto) {
         dto.setPassword(encoder.encode(dto.getPassword()));
-        BeanUtils.copyProperties(repository.save(new Usuario(dto)), dto);
-        return dto;
+        return new UsuarioDto(repository.save(new Usuario(dto)));
     }
 }
---
+++ ms-user/src/test/java/br/com/fatec/user/service/UserServiceTest.java
@@ -0,0 +1,103 @@
+package br.com.fatec.user.service;
+
+import br.com.fatec.user.model.Usuario;
+import br.com.fatec.user.model.dto.UsuarioDto;
+import br.com.fatec.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+    @Mock
+    private BCryptPasswordEncoder encoder;
+    @Mock
+    private UserRepository userRepository;
+
+    private Usuario usuario;
+    private UsuarioDto dto;
+
+    @BeforeEach
+    void setUp() {
+        initMocks(this);
+        StartObjects();
+    }
+
+    @Test
+    void whenFindByIdThenReturnUser() {
+        when(userRepository.findById(any())).thenReturn(Optional.of(usuario));
+        Usuario resp = userService.findById(any());
+        assertEquals(Usuario.class, resp.getClass());
+    }
+
+    @Test
+    void whenFindByIdThenReturnRuntimeException() {
+        when(userRepository.findById(any())).thenThrow(new RuntimeException());
+        try {
+            userService.findById(any());
+        } catch (Exception e) {
+            assertEquals(RuntimeException.class, e.getClass());
+        }
+    }
+
+    @Test
+    void whenFindAllThenReturnListOfUsers() {
+        when(userRepository.findAll()).thenReturn(List.of(usuario));
+        List<UsuarioDto> resp = userService.findAll();
+        assertEquals(UsuarioDto.class, resp.get(0).getClass());
+    }
+
+    @Test
+    void whenFindByEmailThenReturnUser() {
+        when(userRepository.findByEmail(any())).thenReturn(Optional.of(usuario));
+        Usuario resp = userRepository.findByEmail(any()).get();
+        assertEquals(usuario.getEmail(), resp.getEmail());
+    }
+
+    @Test
+    void whenFindByEmailThenReturnRuntimeException() {
+        when(userRepository.findByEmail(any())).thenThrow(new RuntimeException());
+        try {
+            userService.findByEmail(any());
+        } catch (Exception e) {
+            assertEquals(RuntimeException.class, e.getClass());
+        }
+    }
+
+    @Test
+    void whenCreateThenReturnUserCreated() {
+        when(userRepository.save(any())).thenReturn(usuario);
+        UsuarioDto resp = userService.create(dto);
+        assertEquals(usuario.getId(), resp.getId());
+    }
+
+    private void StartObjects() {
+        usuario = Usuario.builder()
+                .id(1L)
+                .name("Fulano")
+                .email("fulano@fulano.com")
+                .password("123")
+                .roles(new HashSet<>())
+                .build();
+
+        dto = UsuarioDto.builder()
+                .id(1L)
+                .name("Fulano")
+                .email("fulano@fulano.com")
+                .password("123")
+                .roles(new HashSet<>())
+                .build();
+    }
+}
